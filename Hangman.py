e9e5c8b517d7df54d1ff5ee37c60d4852fd79032
diegogv <diegogv@gmail.com>
2023-10-15 14:13:20 -0300

    jogo de Hangman

---
+++ Hangman.py
@@ -0,0 +1,39 @@
+import random
+import string
+from words import palavras
+
+def get_valid(palavras): 
+    palavra =  random.choice(palavras) #Pega uma palavra aleatoria
+    while '-' in palavra or ' ' in palavra: 
+        palavra = random.choice(palavras)
+    return palavra.upper()
+
+def hagman(): 
+    palavra = get_valid(palavras)
+    letras_palavra = set(palavra) #Letras de uma palavra
+    alfabeto = set(string.ascii_uppercase)
+    letras_utilizadas = set() #quais Letras foram Adivinhadas
+
+    #Entrada de dados
+    while len(letras_palavra) > 0: 
+        # Letras Utilizadas
+        print(f'Voce ja utilizou as letras:', ' '.join(letras_utilizadas))
+
+        #Palavra Atual
+        word_list = [letter if letter in letras_utilizadas else '-' for letter in palavra]
+        print(f'Palavra atual: ', ' '.join(word_list))
+
+        letra_usuario = input('Adivinhe uma Letra: ').upper()
+        if letra_usuario in alfabeto - letras_utilizadas: 
+            letras_utilizadas.add(letra_usuario)
+            if letra_usuario in letras_palavra:
+                letras_palavra.remove (letra_usuario)
+        elif letra_usuario in letras_utilizadas:
+            print('Voce já utilizadou esta Letra, Tente outra Vez como diria Raul!! ')
+        else: 
+            print('Letra Invalida')    
+
+    print(f'Parabens vc acertou a palavra é {palavra}')
+#user_input = input('Digite uma Letra: ')
+hagman()
+#print(user_input)
